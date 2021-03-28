84c96aa58df23bde81eff46b998c4d69d08d2b1b
Dinara9595 <dinara.petuhova95@yandex.ru>
2021-03-28 05:17:20 +0300

    Finish user signup

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -44,7 +44,7 @@ class UserTest < ActiveSupport::TestCase
 
   test "email validation should reject invalid addresses" do
     invalid_addresses =%w[user@example,com user_at_foo.org user.name@example.
-                           foo@bar_baz.com foo@bar+baz.com]
+                           foo@bar_baz.com foo@bar+baz.com foo@bar..com]
     invalid_addresses.each do |invalid_addresses|
       @user.email = invalid_addresses
       assert_not @user.valid?, "#{invalid_addresses.inspect} should be valid"
@@ -66,5 +66,12 @@ class UserTest < ActiveSupport::TestCase
   test "password should have a minimum length" do
     @user.password = @user.password_confirmation = "a" * 5
     assert_not @user.valid?
+  end
+
+  test "email addresses should be saved as lower-case" do
+    mixed_case_email = "Foo@ExAMPle.CoM"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
   end
 end
